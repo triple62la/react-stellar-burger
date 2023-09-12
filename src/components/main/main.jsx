2b7b61cac4e9f421caad6406bdd11d1caefe80ef
rakhimzhon.dovidov <rkdovidov@rt-dc.ru>
2023-09-12 17:19:12 +0300

    feat: add modals and fetch resourses and preloader

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -5,16 +5,16 @@ import {BurgerConstructor} from "./burger-constructor/burger-constructor";
 import {Tabs} from "./tabs/tabs";
 import {data} from "../../utils/data";
 
-export const Main = ()=>(
+export const Main = ({ingredients})=>(
     <main className={classes.main}>
         <h1 className={clsx(classes.title , "text text_type_main-large pt-10")}>Соберите бургер</h1>
         <section className={classes.section}>
             <div className={classes.column}>
                 <Tabs/>
-                <BurgerIngredients data={data}/>
+                <BurgerIngredients data={ingredients}/>
             </div>
             <div className={classes.column}>
-                <BurgerConstructor data = {data}/>
+                <BurgerConstructor data = {ingredients}/>
             </div>
         </section>
     </main>
